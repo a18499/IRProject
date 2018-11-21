d7a66bd30b6d8689eb0cc5211407d6dbb5ef2516
little-nerd <a184999@gmail.com>
2018-11-21 22:39:13 +0800

    change to single thread

--- Test.py
+++ Test.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     docVector = allComment
     #gvsm.initi(docVector, query_vector)
     #gvsm.mainProcess()
-    calculateJob(query_vector, docVector)
+    calculateJob(docVector, query_vector)
     #t = threading.Thread(target= calculateJob(query_vector, docVector))
     #t.start()
     #threadPool.append(t)
