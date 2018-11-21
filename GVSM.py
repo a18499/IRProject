29c7e7855c6fc5e94a8d069008b722907d6455c9
little-nerd <a184999@gmail.com>
2018-11-21 23:57:55 +0800

    change to 26

--- GVSM.py
+++ GVSM.py
@@ -124,7 +124,7 @@ class GVSM:
         print("corpus_tfidf_matrix: " + str(corpus_tfidf_matrix.toarray()))
         tot_words = len(tfidf_vectorizer.vocabulary_)
         print('tot_words ' + str(tot_words))
-        if(tot_words > 30):
+        if(tot_words > 26):
             return "false"
         test = tfidf_vectorizer.transform(queryList)  # turn into document-term matrix (tf-idf)
 
